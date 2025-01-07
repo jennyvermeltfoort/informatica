529342d1d094efafa19174685d941117d1b376b8
Jenny Vermeltfoort <jennyvermeltfoort@outlook.com>
2025-01-07 12:46:24 +0100

    print

--- src/main.c
+++ src/main.c
@@ -48,14 +48,14 @@ segment_t* world = buffer_1;
 segment_t* world_buffer = buffer_2;
 // stub. ZII
 char newline_table[SIZE_WORLD];
-char segment_format[1 << 8][9];
+char segment_format[1 << 8][8];
 // 0 1 2
 // 3 4 5
 // 6 7 8
 cell_value_t bitmap_translator[1 << 9];
 
-uint8_t get_index(const cell_segment_rw_t rw, const uint16_t x,
-                  const uint16_t y) {
+inline uint8_t get_index(const cell_segment_rw_t rw, const uint16_t x,
+                         const uint16_t y) {
     uint8_t oob =
         (y >= SIZE_WORLD_Y) || (x / SIZE_SEGMENT >= SIZE_WORLD_X);
     uint8_t index = (SIZE_WORLD_X * y + x / SIZE_SEGMENT) * !oob +
@@ -63,13 +63,15 @@ uint8_t get_index(const cell_segment_rw_t rw, const uint16_t x,
     return index;
 }
 
-segment_t get_cell_segment(const segment_t* const buf,
-                           const uint16_t x, const uint16_t y) {
+inline segment_t get_cell_segment(const segment_t* const buf,
+                                  const uint16_t x,
+                                  const uint16_t y) {
     return buf[get_index(cell_segment_read, x, y)];
 }
 
-void set_cell_segment(segment_t* const buf, const segment_t segment,
-                      const uint16_t x, const uint16_t y) {
+inline void set_cell_segment(segment_t* const buf,
+                             const segment_t segment,
+                             const uint16_t x, const uint16_t y) {
     buf[get_index(cell_segment_write, x, y)] = segment;
 }
 
@@ -82,38 +84,18 @@ void set_cell_value(segment_t* const buf, const cell_value_t value,
     set_cell_segment(buf, segment, x, y);
 }
 
-cell_value_t get_cell_value(const segment_t* const buf,
-                            const int16_t x, const uint16_t y) {
-    return (cell_value_t)(get_cell_segment(buf, x, y) >>
-                              (x % SIZE_SEGMENT) &
-                          0x1);
-}
-
-uint8_t calc_neighbours(const segment_t* const buf, const uint16_t x,
-                        const uint16_t y) {
-    return get_cell_value(buf, x - 1, y - 1) +
-           get_cell_value(buf, x, y - 1) +
-           get_cell_value(buf, x + 1, y - 1) +
-           get_cell_value(buf, x - 1, y) +
-           get_cell_value(buf, x + 1, y) +
-           get_cell_value(buf, x - 1, y + 1) +
-           get_cell_value(buf, x, y + 1) +
-           get_cell_value(buf, x + 1, y + 1);
-}
-
 void print_world(void) {
+    char buf[SIZE_WORLD * (8 * 4 + 1)];
+    char* ptr = buf;
     for (uint8_t i = 0; i < SIZE_WORLD; i++) {
         segment_t segment = world[i];
-        printf("%c", newline_table[i]);
-        printf("%s", segment_format[(segment)&0XFF]);
-        printf("%s", segment_format[(segment >>= 8) & 0XFF]);
-        printf("%s", segment_format[(segment >>= 8) & 0XFF]);
-        printf("%s", segment_format[(segment >>= 8) & 0XFF]);
-        printf("%s", segment_format[(segment >>= 8) & 0XFF]);
-        printf("%s", segment_format[(segment >>= 8) & 0XFF]);
-        printf("%s", segment_format[(segment >>= 8) & 0XFF]);
-        printf("%s", segment_format[(segment >>= 8) & 0XFF]);
+        ptr += sprintf(ptr, "%c%.8s%.8s%.8s%.8s", newline_table[i],
+                       segment_format[(segment)&0XFF],
+                       segment_format[(segment >> 8) & 0XFF],
+                       segment_format[(segment >> 16) & 0XFF],
+                       segment_format[(segment >> 24) & 0XFF]);
     }
+    fwrite(buf, 1, ptr - buf, stdout);
 }
 
 void generate_segment_format(const char alive, const char dead) {
@@ -126,7 +108,6 @@ void generate_segment_format(const char alive, const char dead) {
         segment_format[i][5] = (i & (1 << 5)) ? alive : dead;
         segment_format[i][6] = (i & (1 << 6)) ? alive : dead;
         segment_format[i][7] = (i & (1 << 7)) ? alive : dead;
-        segment_format[i][8] = '\0';
     }
 }
 
@@ -150,44 +131,49 @@ void generate_bitmap_translation_table(void) {
 segment_t generate_segment(segment_t* buf, uint8_t x, uint8_t y) {
     uint8_t oyt = y - 1;
     uint8_t oyb = y + 1;
+    uint16_t bitmaps[SIZE_SEGMENT] = {};
 
     segment_t segment_top = get_cell_segment(buf, x, oyt);
     segment_t segment_middle = get_cell_segment(buf, x, y);
     segment_t segment_bottom = get_cell_segment(buf, x, oyb);
 
-    segment_t attach_segment_top = get_cell_segment(buf, x - 1, oyt);
-    segment_t attach_segment_middle = get_cell_segment(buf, x - 1, y);
-    segment_t attach_segment_bottom =
-        get_cell_segment(buf, x - 1, oyb);
+    segment_t left_segment_top = get_cell_segment(buf, x - 1, oyt);
+    segment_t left_segment_middle = get_cell_segment(buf, x - 1, y);
+    segment_t left_segment_bottom = get_cell_segment(buf, x - 1, oyb);
 
-    uint16_t bitmap = ((attach_segment_top >> 31) & 0x1) |
-                      ((segment_top & 0x3) << 1) |
-                      (((attach_segment_middle >> 31) & 0x1) << 3) |
-                      ((segment_middle & 0x3) << 4) |
-                      (((attach_segment_bottom >> 31) & 0x1) << 6) |
-                      ((segment_bottom & 0x3) << 7);
-    segment_t segment = bitmap_translator[bitmap];
+    segment_t right_segment_top = get_cell_segment(buf, x + 1, oyt);
+    segment_t right_segment_middle = get_cell_segment(buf, x + 1, y);
+    segment_t right_segment_bottom =
+        get_cell_segment(buf, x + 1, oyb);
 
-    for (uint8_t i = 1; i < 31; i++) {
-        bitmap = ((segment_top)&0x7) | (((segment_middle)&0x7) << 2) |
-                 (((segment_bottom)&0x7) << 5);
-        segment |= ((uint32_t)bitmap_translator[bitmap] << i);
+    bitmaps[0] =
+        ((left_segment_top >> (SIZE_SEGMENT - 1)) & 0x1) |
+        ((segment_top & 0x3) << 1) |
+        (((left_segment_middle >> (SIZE_SEGMENT - 1)) & 0x1) << 3) |
+        ((segment_middle & 0x3) << 4) |
+        (((left_segment_bottom >> (SIZE_SEGMENT - 1)) & 0x1) << 6) |
+        ((segment_bottom & 0x3) << 7);
+
+    for (uint8_t i = 1; i < (SIZE_SEGMENT - 1); i++) {
+        bitmaps[i] = ((segment_top)&0x7) |
+                     (((segment_middle)&0x7) << 2) |
+                     (((segment_bottom)&0x7) << 5);
         segment_top >>= 1;
         segment_middle >>= 1;
         segment_bottom >>= 1;
     }
 
-    attach_segment_top = get_cell_segment(buf, x + 1, oyt);
-    attach_segment_middle = get_cell_segment(buf, x + 1, y);
-    attach_segment_bottom = get_cell_segment(buf, x + 1, oyb);
+    bitmaps[SIZE_SEGMENT - 1] = ((segment_top)&0x3) |
+                                ((right_segment_top & 0x1) << 2) |
+                                (((segment_middle)&0x3) << 3) |
+                                ((right_segment_middle & 0x1) << 5) |
+                                (((segment_bottom)&0x3) << 6) |
+                                ((right_segment_bottom & 0x1) << 8);
 
-    bitmap = ((segment_top)&0x3) | ((attach_segment_top & 0x1) << 2) |
-             (((segment_middle)&0x3) << 3) |
-             ((attach_segment_middle & 0x1) << 5) |
-             (((segment_bottom)&0x3) << 6) |
-             ((attach_segment_bottom & 0x1) << 8);
-    segment |= ((uint32_t)bitmap_translator[bitmap] << 31);
-
+    segment_t segment = 0;
+    for (uint8_t i = 0; i < SIZE_SEGMENT; i++) {
+        segment |= ((uint32_t)bitmap_translator[bitmaps[i]] << i);
+    }
     return segment;
 }
 
@@ -198,11 +184,6 @@ void generate_iteration(void) {
 
     for (uint8_t y = 0; y < SIZE_WORLD_Y; y++) {
         for (uint8_t x = 0; x < SIZE_WORLD_X; x++) {
-            // uint8_t neighbours = calc_neighbours(world_buffer, x,
-            // y); cell_value_t value = get_cell_value(world_buffer,
-            // x, y); set_cell_value(world,
-            // cell_rules[value][neighbours], x,
-            //                y);
             segment_t segment = generate_segment(world_buffer, x, y);
             set_cell_segment(world, segment, x, y);
         }
